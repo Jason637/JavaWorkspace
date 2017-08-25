f924bd56b11d7c07d59c3a7c417ac8ef18bd37f2
GOEBEL <SJGOEBEL@caddoschools.org>
2017-08-25 09:17:46 -0500

    Added a scanner

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/42/d082dcdc998900171e6bfc6232d848ed
@@ -0,0 +1,11 @@
+package basicInputOutput.controller;
+
+public class BasicIOController 
+{
+	public void printGreeting()
+	{
+		String firstName = "Stephen";
+		String lastName = "Goebel";
+		System.out.println(firstName + " " + lastName);
+	}
+}
--- LearningAboutStrings/src/basicInputOutput/controller/BasicIOController.java
+++ LearningAboutStrings/src/basicInputOutput/controller/BasicIOController.java
@@ -1,11 +1,25 @@
 package basicInputOutput.controller;
+import java.util.Scanner;
 
 public class BasicIOController 
 {
+	private String firstName = "";
+	private String lastName = "";
+	
+
+	public BasicIOController(String first, String last)
+	{
+		firstName = first; 
+		lastName = last;
+	}
+	
 	public void printGreeting()
 	{
-		String firstName = "Stephen";
-		String lastName = "Goebel";
-		System.out.println(firstName + " " + lastName);
+		System.out.println("Hello " + firstName + " " + lastName);
+		Scanner myScanner = new Scanner(System.in);
+		String response = myScanner.nextLine();
+		System.out.println(response);
+		myScanner.close();
+		
 	}
 }
